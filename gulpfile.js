652de909a5d72dcf268ede519d45b50b344fdd8e
Dariusz Grabka <dariusz@tulip.io>
2017-08-01 18:07:07 -0400

    Adding gulp tasks for minifying, creating dist

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+
+gulp.task('default', function () {
+});
+
+gulp.task('images', function () {
+	return gulp.src('src/img/**')
+		.pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('useref', function () {
+	return gulp.src('src/*.html')
+		.pipe(useref())
+		.pipe(gulpIf('*.js', uglify()))
+		.pipe(gulpIf('*.css', cssnano()))
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean', function () {
+	return del.sync('dist');
+})

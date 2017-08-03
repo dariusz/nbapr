3ca5ff879be4980b1f14fc8f6eb4abb0a42aae92
Dariusz Grabka <dariusz@tulip.io>
2017-08-02 22:19:56 -0400

    min and max indicators

--- src/js/transformer.js
+++ src/js/transformer.js
@@ -9,6 +9,30 @@ var transformer = {
 		return week.toLowerCase();
 	},
 
+	"getmaxfromrow" : function (row) {
+		var max = 0;
+		var val;
+		for (idx in row) {
+			if (~idx.indexOf("gsx$")) {
+				val = parseFloat(row[idx]['$t']);
+				if (val > max) max = val;
+			}
+		}
+		return max;
+	},
+
+	"getminfromrow" : function (row) {
+		var min = 99999999;
+		var val;
+		for (idx in row) {
+			if (~idx.indexOf("gsx$")) {
+				val = parseFloat(row[idx]['$t']);
+				if (val < min) min = val;
+			}
+		}
+		return min;
+	},
+
 	"weeklysources" : function (input, weekname) {
 		var data = { "sources" : {} }
 		var rows = input.feed.entry;
@@ -44,38 +68,56 @@ var transformer = {
 			// each row is a specific measure for a specific week
 
 			var row = rows[rowkey];
-			var measure = row["gsx$measure"]['$t']; delete row["gsx$measure"];
 			var week = transformer.getweekfromrow(row); delete row["gsx$week"];
-			measure = measure.toLowerCase();
 
 			// only get the requested week
 
 			if (weekname != week)
 				continue;
 
+			var measure = row["gsx$measure"]['$t']; delete row["gsx$measure"];
+			var max = transformer.getmaxfromrow(row);
+			var min = transformer.getminfromrow(row);
+			measure = measure.toLowerCase();
+
 			// go through each column (aka team)
 
 			for (idx in row) {
 				if (~idx.indexOf("gsx$")) { // our cell values are prefixed with gsx$
+
 					var team = idx.substr(4);
+					var val = parseFloat(row[idx]['$t']);
+
 					if (typeof data[team] == "undefined") data[team] = { "team" : team }
-					data[team][measure] = parseFloat(row[idx]['$t']);
+					data[team][measure] = val;
+
+					if (val == max) {
+						data[team]["max_" + measure] = true
+					}
+
+					if (val == min) {
+						data[team]["min_" + measure] = true
+					}
 				}
 			}
 		}
 
-		// sort each team entry by rank
+		// calculate the sort order (rank w/ stdev)
 
 		var rankeddata = { }
 		for (idx in data) {
 			var rank = data[idx]['rank'] + (data[idx]['stdev']/1000);
 			rankeddata[rank] = data[idx];
 		}
+		delete data;
+
+		// sort the list
 
 		var sorted = { "teams" : {} }
 		Object.keys(rankeddata).sort(transformer.sortNumber).forEach(function(key) {
 			sorted['teams'][key] = rankeddata[key];
 		});
+		delete rankeddata;
 
 		return sorted;
 	}

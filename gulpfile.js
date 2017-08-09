68e2425d468e716f6b5800d89c664b8961445a5a
Dariusz <dariusz@grabka.org>
2017-08-08 23:54:07 -0400

    Adding local versions of data files

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var del = require('del');
 var runSequence = require('run-sequence');
+var download = require("gulp-download-stream");
 
 gulp.task('default', function () {
 });
@@ -32,5 +33,19 @@ gulp.task('clean', function () {
 })
 
 gulp.task('build', function(callback) {
-	runSequence('clean', ['images', 'useref', 'vendor'], callback);
+	runSequence('clean', 'dl_ranks', 'dl_sources', ['images', 'useref', 'vendor'], callback);
+});
+
+gulp.task('dl_ranks', function() {
+	return download({
+		"url": "https://spreadsheets.google.com/feeds/list/1nK_9dsvKj4F22hqJPFJOBE-ftTVMac3vFIkp9ULjSbo/1/public/values?alt=json-in-script&callback=nbapr.loadRankings",
+		"file": "rankings.js"
+	}).pipe(gulp.dest("dist/data/"));
+});
+
+gulp.task('dl_sources', function () {
+	return download({
+		"url": "https://spreadsheets.google.com/feeds/list/1nK_9dsvKj4F22hqJPFJOBE-ftTVMac3vFIkp9ULjSbo/2/public/values?alt=json-in-script&callback=nbapr.loadSources",
+		"file": "sources.js"
+	}).pipe(gulp.dest("dist/data/"));
 });
